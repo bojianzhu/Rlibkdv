20d45e719c15ef682fe0db3d822147767c0f2cf7
bojianzhu <bjzhu999@gmail.com>
2023-08-08 17:21:10 +0800

    add Rmd

---
+++ R/packagegename-data.R
@@ -0,0 +1,11 @@
+#' Hong Kong COVID-19 Cases Dataset
+#'
+#' This dataset contains the COVID-19 cases data in Hong Kong.
+#'
+#' @format A data frame with 3 variables:
+#' \describe{
+#'   \item{lon}{Longitude of the location}
+#'   \item{lat}{Latitude of the location}
+#'   \item{t}{Number of COVID-19 cases}
+#' }
+"hk"
